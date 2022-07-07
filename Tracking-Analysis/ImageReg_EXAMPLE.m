22dd94786c172e8e3adb4656714f0d3ab9fb5c27
Jingyang Zheng <107644381+jingyangzheng@users.noreply.github.com>
2022-07-07 14:56:06 -0400

    Add files via upload

--- Tracking-Analysis/ImageReg_EXAMPLE.m
+++ Tracking-Analysis/ImageReg_EXAMPLE.m
@@ -28,50 +28,81 @@
     % (https://www.biorxiv.org/content/10.1101/2022.06.12.495830v2). The authors would appreciate if any users 
     % could email the copyright holder (jz848@cornell.edu) so that the copyright holder can share and cite examples of adaptations.
 
+clearvars
+folder = 'D:\Jingyang\Cartilage\Confocal_Images\';
+date = '22_04_29';
+mkdir(strcat(folder, date, '\pos1_reg\'));
+num_frames = 840;
 
+LongPath = strcat(folder, date, '\pos1\long'); % this is where the images for the 'long' tracking are stored
+WritePath = strcat(folder, date, '\pos1_reg\images\'); % this is where the registered images should be saved
+mkdir(WritePath);
 
-LongPath = ''; % this is where the images for the 'long' tracking are stored
-WritePath = ''; % this is where the registered images should be saved
+% load image
+% Obtain info from image
+info = imfinfo(fullfile(folder,date,'\pos1.tif')); % gives info about tiff stack
+rowdim = info.Height;
+coldim = info.Width;
+total_imgs = numel(info);
+num_frames = total_imgs;
+% read in tiff stack, save to stack1
+stack1 = zeros(rowdim, coldim, num_frames, 'uint8');
+stack_red = zeros(rowdim, coldim, num_frames, 'uint8');
+stack_green = zeros(rowdim, coldim, num_frames, 'uint8');
+stack_blue = zeros(rowdim, coldim, num_frames, 'uint8');
+for k = 1 : total_imgs
+    temp_tiff = imread(fullfile(folder,date,'\pos1.tif'), k);
+    stack_red(:,:,k) = temp_tiff(:,:,1);
+    stack_green(:,:,k) = temp_tiff(:,:,2);
+    stack_blue(:,:,k) = temp_tiff(:,:,3);
+    stack1(:,:,k) = temp_tiff(:,:,1)+temp_tiff(:,:,2)+temp_tiff(:,:,3);
+end
 
-fixedpts = NaN(size(track_array,2)/2,2);
-movingpts = NaN(size(track_array,2)/2,2);
+load(strcat(folder,date,'\pos1\pos1_locs.mat'));
+fixedpts = NaN(size(filled_long,2)/2,2);
+movingpts = NaN(size(filled_long,2)/2,2);
 
-for i = 1:(size(track_array,2)/2)
-    fixedpts(i,1) = track_array(1,i*2-1);
-    fixedpts(i,2) = track_array(1,i*2);
+for i = 1:(size(filled_long,2)/2)
+    fixedpts(i,1) = filled_long(1,i*2-1);
+    fixedpts(i,2) = filled_long(1,i*2);
 end
 
 % initialize registered arrays
 blue_reg = zeros(512,512,num_frames,'uint8');
 red_reg = zeros(512,512,num_frames,'uint8');
 green_reg = zeros(512,512,num_frames,'uint8');
+total_reg = zeros(512,512,3,num_frames,'uint8');
 
 % % read first image in long tracking
-I = imread(fullfile(LongPath,strcat(video_type,'_filled1.tif')));
+I = imread(fullfile(LongPath,strcat('pos1_filled1.tif')));
 % % write images
 % imwrite(I,fullfile(WritePath,strcat(video_type,'_tform.tif')));
-imwrite(stack_blue(:,:,1),fullfile(WritePath,strcat(video_type,'_tform_clean_blue.tif')));
-imwrite(stack_red(:,:,1),fullfile(WritePath,strcat(video_type,'_tform_clean_red.tif')));
-imwrite(stack_green(:,:,1),fullfile(WritePath,strcat(video_type,'_tform_clean_green.tif')));
+imwrite(stack_blue(:,:,1),fullfile(WritePath,'pos1_tform_blue.tif'));
+imwrite(stack_green(:,:,1),fullfile(WritePath,'pos1_tform_green.tif'));
+imwrite(stack_red(:,:,1),fullfile(WritePath,'pos1_tform_red.tif'));
 
 % apply imwarp and save as an array instead of writing it
 for j = 1:num_frames
     warning('off','all')
-    J = imread(fullfile(LongPath,strcat(video_type,'_filled',num2str(j),'.tif')));
-    for i = 1:(size(track_array,2)/2)
-        movingpts(i,1) = track_array(j,i*2-1);
-        movingpts(i,2) = track_array(j,i*2);
+    J = imread(fullfile(LongPath,strcat('pos1_filled',num2str(j),'.tif')));
+    for i = 1:(size(filled_long,2)/2)
+        movingpts(i,1) = filled_long(j,i*2-1);
+        movingpts(i,2) = filled_long(j,i*2);
     end
     tform = fitgeotrans(movingpts,fixedpts,'lwm',15);
     blue_reg(:,:,j) = imwarp(stack_blue(:,:,j),tform,'OutputView',imref2d(size(I)));
-    imwrite(blue_reg(:,:,j),fullfile(WritePath,strcat(video_type,'_tform_clean_blue.tif')),'Writemode','append');
+    imwrite(blue_reg(:,:,j),fullfile(WritePath,'pos1_tform_blue.tif'),'Writemode','append');
     green_reg(:,:,j) = imwarp(stack_green(:,:,j),tform,'OutputView',imref2d(size(I)));
-    imwrite(green_reg(:,:,j),fullfile(WritePath,strcat(video_type,'_tform_clean_green.tif')),'Writemode','append');
+    imwrite(green_reg(:,:,j),fullfile(WritePath,'pos1_tform_green.tif'),'Writemode','append');
     red_reg(:,:,j) = imwarp(stack_red(:,:,j),tform,'OutputView',imref2d(size(I)));
-    imwrite(red_reg(:,:,j),fullfile(WritePath,strcat(video_type,'_tform_clean_red.tif')),'Writemode','append');
+    imwrite(red_reg(:,:,j),fullfile(WritePath,'pos1_tform_red.tif'),'Writemode','append');
+    total_reg(:,:,1,j) = red_reg(:,:,j);
+    total_reg(:,:,2,j) = green_reg(:,:,j);
+    total_reg(:,:,3,j) = blue_reg(:,:,j);
+    imwrite(red_reg(:,:,j),fullfile(WritePath,'pos1_tform_total.tif'),'Writemode','append');
 end
 
 % save matrices
-save(fullfile(WritePath,strcat(video_type,'_blue_reg.mat')),'blue_reg');
-save(fullfile(WritePath,strcat(video_type,'_green_reg.mat')),'green_reg');
-save(fullfile(WritePath,strcat(video_type,'_red_reg.mat')),'red_reg');
+save(fullfile(WritePath,'pos1_blue_reg.mat'),'blue_reg');
+save(fullfile(WritePath,'pos1_green_reg.mat'),'green_reg');
+save(fullfile(WritePath,'pos1_red_reg.mat'),'red_reg');
